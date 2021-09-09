bc39134554f662d45cdd7f8ef9ebd44bf754865e
Mitchell Arnold <marnold@appddictionstudio.com>
2021-09-09 09:44:15 -0500

    initial Nurse Site layout

---
+++ nurse.cmd
@@ -0,0 +1,7 @@
+git init 
+
+git add -A 
+git commit -m "initial Nurse Site layout " 
+git branch -M master 
+git remote add origin https://github.com/MitchellArnold/NurseAmerica.git 
+git push -u origin master
